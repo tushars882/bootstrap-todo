9691c6b47ade3796752a0489b3046741fc7e53c4
tushars882 <tushars882@gmail.com>
2022-06-29 13:54:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './App.css'
+import './index.css'
+export default class App extends Component {
+  render() {
+    return (
+      <div className='container-fluid my-5'>
+        <div className="row">
+          <div className="col-sm-6 mx-auto">
+            <h2 className='text-center text-white shadow-lg p-3'>Today's plan</h2>
+            <div className="row">
+              <div className="col-9">
+                <input type="text" placeholder='Add Your Plan' className='form-control' />
+              </div>
+              <div className="col-2">
+                <button className='btn btn-warning px-3 fw-bold'>
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
